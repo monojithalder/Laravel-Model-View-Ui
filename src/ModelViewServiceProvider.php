43a179577e214493a5110a1bbc9d1c02953d0d3f
monojithalder <monojithalder@hotmail.com>
2017-04-29 12:38:31 +0530

    Initial Commit

---
+++ src/ModelViewServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Novatree\ModelViewUi;
+
+use Illuminate\Support\ServiceProvider;
+use App;
+
+class ModelViewServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/migrations' => base_path('database/migrations'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes.php';
+    }
+}
